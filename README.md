8b9b266b412a4d8760740ae325164208c8d92af1
Sasa Prsic <sasa@basiq.io>
2020-09-04 15:15:50 +0200

    Add build sdk script.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # basiq-swagger
 Basiq API documentation.
+
+
+To build sdk use ./build.sh script. Before building the sdk make sure you have goswagger installed (https://goswagger.io).
+
+For this generation to compile you need to have some packages in your GOPATH:
+
+        * github.com/go-openapi/errors
+        * github.com/go-openapi/runtime
+        * github.com/go-openapi/runtime/client
+        * github.com/go-openapi/strfmt
--- build.sh
+++ build.sh
@@ -1,53 +1,20 @@
 #!/bin/bash
 
-if [ $# -eq 0 ]; then
-  echo "No arguments supplied; add version number e.g ./build 2.0.0"
-  exit
-fi
+RELEASE_VERSION=$(grep -o '[0-9]\.[0-9]\.[0-9]' ./swagger.json)
 
-# get swagger version file
-RELEASE_VERSION=$1
+BUILD_DIR="./dist"
 
-#create dir if not exists
-if [ -d "./basiq-swagger-ui" ]; then
-  echo "Directory /basiq-swagger-ui exists."
+if [ -d "$BUILD_DIR" ]; then
+  echo "Directory  exists."
 else
   echo "Creating directory"
-  cp -r ./swagger-ui ./basiq-swagger-ui
+  mkdir "$BUILD_DIR"
 fi
 
-# build a version from a template
-cp ./swagger.json ./basiq-swagger-ui/swagger-$RELEASE_VERSION.json
+swagger generate client -f ./swagger.json -A ./basiq-"$RELEASE_VERSION"
 
-sed -i "s/{{version}}/$RELEASE_VERSION/g" ./basiq-swagger-ui/swagger-$RELEASE_VERSION.json
-
-# generate go client
-swagger generate client -f ./swagger.json -A ./basiq-$RELEASE_VERSION
-zip -r basiq-sdk-$RELEASE_VERSION.zip ./client ./models
-mv basiq-sdk-$RELEASE_VERSION.zip ./basiq-swagger-ui
+mv ./client "$BUILD_DIR"
+mv ./models "$BUILD_DIR"
 rm -rf ./client ./models
 
-cp ./logo.svg ./basiq-swagger-ui
-
-cd ./basiq-swagger-ui
-
-# append all swagger versions
-i=0
-for f in $(ls swagger-*.json | sort -r); do
-  version=$(echo $f | grep -o -E '[0-9]+\.[0-9]+\.[0-9]+')
-  if [[ $i == 0 ]]; then
-    item=$item"{url:"\"$f\"",name:"\"$version\""}"
-  else
-    item=$item",{url:"\"$f\"",name:"\"$version\""}"
-  fi
-  i=$(expr $i + 1)
-done
-url=$item
-url="["$url"]"
-cd ..
-cp ./swagger-ui/index.html ./basiq-swagger-ui
-#
-cd ./basiq-swagger-ui
-sed -i "s/{{swagger-versions}}/$url/g" ./index.html
-
-echo "Done generating $RELEASE_VERSION swagger documentation. Check ./basiq-swagger-ui"
+echo "Done generating $RELEASE_VERSION swagger sdk. Check "$BUILD_DIR
